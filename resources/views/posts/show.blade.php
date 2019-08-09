caee162236507b776e3dbf0e5ad0e6e77efa5894
Marco Huerta <marco.huerta.sc@itszapopan.edu.mx>
2019-08-09 18:55:56 -0500

    :wolf: Aplication: Implementing comment list :wolf:

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,4 +9,22 @@
 @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 5 )
 <strong>New!</strong>
 @endif
+
+<h4>Comments</h4>
+<!--Esta parte del forelse se trae select en el show para mostrar los datos de la DB-->
+@forelse ($post->comments as $comment)
+<p>
+    {{ $comment->content }}
+</p>
+
+<P class="text-muted">
+     added {{ $comment->created_at->diffForHumans()}}
+</P>
+
+@empty
+
+<p>No comments yet!</p>
+
+@endforelse
+<!--.FORELSE-->
 @endsection('content')
